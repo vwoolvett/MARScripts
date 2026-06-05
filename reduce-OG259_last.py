50efd22c8f6718d7dfcd0b9d0ab6f41548673061
vwoolvett <80853557+vwoolvett@users.noreply.github.com>
2026-06-05 23:21:02 +0200

    added OG259-specific reduction and scansfile

---
+++ reduce-OG259_last.py
@@ -0,0 +1,283 @@
+import copy as copy
+
+# ==== VERSION 2.0: CORRECT RMS MAP HANDLING? ======
+# VWO: solution is to not smooth until exporting
+# a FITS file!
+
+# =================================
+# ==== BEGINNING OF USER INPUT ====
+# =================================
+
+# --- Source and map parameters ---
+source  = 'OG259'         # As in observing logs
+fe      = 'LFA'             # Frontend, either 'LFA' or 'HFA'
+system  = 'GAL'              # Coordinate system for map, 'EQ', 'GAL' or 'HO'
+center  = [259.3, -1.4]            # Center of map in CHOSEN COORDINATES in deg
+sizex   = 1.5               # Size of map in deg for X direction
+sizey   = 1.5               # Size of map in deg for Y direction
+padding = 0.5               # Padding around the map in deg for grid (default ~ 2x array)
+doPlot  = True              # Display maps at each scan. If False, only final
+                            # coadded map per iteration will be displayed.
+
+# ----- Reduction parameters -----
+writeSummary = True         # Write summary of reductions or not
+niters       = 2            # Number of iterations to run, 1 to 3 (recommended 2)
+clip         = 5.           # Sigma clipping level for masking high noise pixels
+flagJumps    = True        # Flag jumps/spikes in the data:
+                            # recommended to set to True for 'weak' sources in LFA
+
+# ----- Scans ------
+# If empty, automatically retrieves all scans of source from Obslogs
+# NOTE: CURRENTLY NOT FUNCTIONAL, PLEASE MANUALLY INPUT SCAN NUMBERS
+scans = [27974,27975,27979,27980,27990,28212,28213,28217,28218,28231,28232,28235,28493,28494,28498,28499,28516,28517,28775,28776]
+
+# Manually exclude bad scans if needed            
+badscans = [27979, 28217, 28498]                   
+
+# ----- Smoothing -----
+smoothby_arcsec = 0.
+
+# ==============================
+# ===== END OF USER INUPUT =====
+# ==============================
+
+
+
+
+
+
+
+
+
+# ===== REDUCTION CODE, DO NOT EDIT BELOW UNLESS YOU KNOW WHAT YOU ARE DOING =====
+# variable checks
+if fe not in ['LFA', 'HFA']:
+    raise ValueError("fe must be either 'LFA' or 'HFA'.")
+if system not in ['EQ', 'GAL', 'HO']:
+    raise ValueError("system must be either 'EQ', 'GAL', or 'HO'.")
+if niters < 1 or niters > 3:
+    raise ValueError("niters must be 1, 2, or 3.")
+
+# find scans if not provided (TO-DO)
+# def findScans(source, fe):
+#if len(scans) == 0:
+#    scans = findScans(source, fe)
+
+# Define myname variable
+myname = str(fe) + "-" + str(source) + "-" + str(system)
+if flagJumps:
+    myname += "-flagJumps"
+
+# Transform 0-360 deg to -180 to 180 if needed
+if center[0] > 180:
+    warn('Center X is greater than 180 deg, transforming to -180 to 180 deg...')
+    center[0] -= 360
+
+# map bounds in absolute EQ or GAL coordinates in deg
+xsize = [center[0] + sizex/2 + padding, center[0] - sizex/2 - padding]
+ysize = [center[1] - sizey/2 - padding, center[1] + sizey/2 + padding]
+
+# Remove bad scans from the list of scans to be reduced
+for badscan in badscans:
+    if badscan in scans:
+        scans.remove(badscan)
+
+
+print('''\
+=====================
+Reduction parameters:
+=====================
+Source:             %s
+Frontend:           %s
+Coordinate system:  %s
+Map center:         %s, %s deg
+Map size (x,y):     %s, %s deg
+Padding:            %s deg
+Map Boundaries:     %s, %s deg in x; %s, %s deg in y
+Iterations:         %i
+Sigmaclip level:    %s
+Flag jumps:         %s
+
+'''%(source, fe, system, center[0], center[1], sizex, sizey, padding,
+     xsize[0], xsize[1], ysize[0], ysize[1], niters, clip, flagJumps))
+
+# Set noPlot
+if not doPlot:
+    noPlot = True
+else:
+    noPlot = False
+
+# Create directory for reduced files if it doesn't exist
+if os.path.exists("ReducedFiles") == False:
+    os.makedirs("ReducedFiles")
+
+# Create directory for reduced files if it doesn't exist
+if writeSummary and os.path.exists("Summaries") == False:
+    os.makedirs("Summaries")
+
+# smoothby
+smoothby_deg = smoothby_arcsec / 3600.
+
+
+# Beginning of reduction loop
+for iter in range(1, niters+1):
+    print("####################################################################")
+    print("####################### Iteration %i starting #######################"%(iter))
+    print("####################################################################")
+
+    if iter == 1:
+        mymodel = None
+        subtract = False
+    else:
+        mymodel = "ReducedFiles/" + str(myname) + "-coadded-flux-iter" + str(iter-1) + ".data"
+        m = restoreFile(mymodel)
+        if iter == 2:
+            subtract = False
+            mymodel = createSourceModel(m, highcut=5.5, lowcut=2.5, sm=smoothby_arcsec, mtype='snr', clip=3)
+            
+        if iter == 3:
+            subtract = True
+            mymodel = createSourceModel(m, highcut=5.5, lowcut=2.5, sm=0., mtype='flux', clip=3)
+            
+    
+    ms=None
+
+    for i,scan in enumerate(scans):
+        scanname = "ReducedFiles/"+str(myname)+"-"+str(scan)+"-iter"+str(iter)+".data"
+        globlist = glob(scanname)
+
+        m = None
+        if len(globlist) ==  0:
+            info('Reducing scan %s (iteration %i)...'%(scan, iter))
+            redweak(scan,fe=fe,size=-1,model=mymodel,subtract=subtract,doPlot=doPlot,extremeFilter=False,writeSummary=writeSummary,flagJumps=flagJumps)
+            
+            # Immediately rename summary and move to new folder
+            if writeSummary:
+                # VWO: made it iteration-specific
+                origname = "%s-%s-%i_summary.txt"%(fe, data.ScanParam.Object, data.ScanParam.ScanNum)
+                newdir = "Summaries/"
+                newname = myname + "-" + str(scan) + "-iter" + str(iter) + "_summary.txt"
+                outname = newdir + newname
+                os.rename(origname, outname)
+
+            # Flagging example to flag a certain time range in a map (seconds from the beining of the scan) 
+            #if scan == 22919: 
+            #    flagMJD(above=1430, below=1600,flag=2)
+
+            # Flagging example to flag a certain tone/KID in a scan
+            #if scan == 28517:
+            #    flagC(3353, flag=2)
+
+            # Create map
+            mapping(oversamp=4,system=system,sizeX=xsize,sizeY=ysize,noPlot=noPlot)
+            
+            # Save map
+            data.Map.dumpMap(scanname)
+
+            # create BoA map
+            m = restoreFile(scanname)
+
+            # For this scan, add non-noisy area and median noise info to summary
+            if writeSummary:
+                # First copy unsmoothed map
+                rmsMap = copy.deepcopy(m)
+
+                # Compute RMS map
+                rmsMap.Data =  1.0 / np.sqrt(rmsMap.Weight)
+
+                # Smooth (if needed) for statistics
+                if smoothby_deg > 0.0:
+                    rmsMap.smoothBy(smoothby_deg)
+
+                # Statistics and write
+                minnoise = np.nanmin(rmsMap.Data)
+                mask = np.where(rmsMap.Data > 5*minnoise)
+                rmsMap.Data[mask] = np.NaN
+                pixelsize = np.abs(rmsMap.WCS['CDELT2'])
+                nrpix = np.sum(~np.isnan(rmsMap.Data))
+                area = nrpix*pixelsize**2
+                noise = np.nanmedian(rmsMap.Data)
+                f = open(outname,'r')
+                lines = f.readlines()
+                f.close()
+                myline = lines[0].replace("\n","")
+                myline = myline+",{:.3f},{:.4f}\n".format(area,noise)
+                f = open(outname,'w')
+                f.write(myline)
+                f.close()
+            
+        else:
+            # Retrieve BoA map
+            info('Reduction for scan %i (iteration %i) found'%(scan, iter))
+            m = restoreFile(scanname)
+
+        if np.all(np.isnan(m.Data)):
+            warn('Map data is all NaNs! Ensure that the map bounds are correct.')
+            break
+        
+        if ms and m:
+            # both are native resolution
+            ms = mapsumfast([ms,m])  
+        
+        elif not ms:
+            # is native resolution
+            ms = copy.deepcopy(m)
+
+        if doPlot:
+            # SNR map creation
+            snrMap = copy.deepcopy(ms)  # Signal
+            snrMap.Data *= np.sqrt(snrMap.Weight)  # SNR = signal * sqrt(weight) = signal / sqrt(noise^2)
+
+            # Smooth (if needed) for display
+            if smoothby_deg > 0.0:
+                snrMap.smoothBy(smoothby_deg)
+
+            # plotting
+            snrMap.display(aspect=1,limitsZ=[-4,12])
+
+    # Iteration complete, now create final coadded maps and display
+    # final SNR map + noise contours with optional clipping of high noise pixels.
+    # RMS map creation
+    rmsMap = copy.deepcopy(ms)  # Signal
+    rmsMap.Data = 1.0 / np.sqrt(rmsMap.Weight)  # Noise = 1/sqrt(weight)
+
+    # SNR map creation
+    snrMap = copy.deepcopy(ms)  # Signal
+    snrMap.Data *= np.sqrt(snrMap.Weight)  # SNR = signal * sqrt(weight) = signal / sqrt(noise^2)
+
+    # Smooth (if needed) for stats and display
+    if smoothby_deg > 0.0:
+        snrMap.smoothBy(smoothby_deg)
+        rmsMap.smoothBy(smoothby_deg)
+
+    # clipping high noise pixels if clip > 0
+    minnoise = np.nanmin(rmsMap.Data)
+    meannoise = np.nanmean(rmsMap.Data)
+    mediannoise = np.nanmedian(rmsMap.Data)
+    
+    if clip > 0:
+        mask = np.where(rmsMap.Data > clip * mediannoise)
+        ms.Data[mask] = np.NaN
+        snrMap.Data[mask] = np.NaN
+        rmsMap.Data[mask] = np.NaN
+
+    # plotting (these are already smoothed)
+    snrMap.display(aspect=0,limitsZ=[-4,12])
+    rmsMap.display(aspect=0,limitsZ=[0, 2*mediannoise],doContour=1,levels=[mediannoise],overplot=1)
+
+    # save full-iteration map without smoothing!
+    # createSourceModel already smooths this, which will be the model
+    outname = "ReducedFiles/"+str(myname)+"-coadded-flux-iter"+str(iter)+".data"  # goes into ReducedFiles dir
+    ms.dumpMap(outname)
+
+    print("################### Iteration %i finished ###################"%(iter))
+    print("minimum noise: %5.1f mJy/b, mean noise: %5.1f mJy/b"%(1000*minnoise,1000*meannoise))
+    print("############################################################")
+
+    # NOTE: VWO: currently, if smooth > 0.0 writeFits2 would
+    # still be computing the RMS wrong...
+    # Smooth (if needed) and export to FITS
+    if smoothby_deg > 0.0:
+        ms.smoothBy(smoothby_deg)
+    outname = str(myname)+"-coadded-iter"+str(iter)+".fits" # Goes into current dir.
+    writeFits2(ms, outfile=outname, overwrite=1)
