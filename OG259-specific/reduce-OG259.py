bab1886fcc58fa6d4493311ae1ced7cd02f84ac0
vwoolvett <80853557+vwoolvett@users.noreply.github.com>
2026-06-08 18:24:58 +0200

    can't ignore errors, code works

--- OG259-specific/reduce-OG259.py
+++ OG259-specific/reduce-OG259.py
@@ -148,10 +148,10 @@ def auxsmoothby(m, Size=smoothby_deg):
     W1 = np.where(V1 > 0.0, 1.0 / V1, 0.0)
 
     # try: rms convolution instead?
-    #V0 = np.where(m.Weight > 0, 1.0 / m.Weight, np.NaN)
-    #ms0 = np.sqrt(V0)
+    #V0 = np.where(m.Weight > 0.0, 1.0 / m.Weight, np.NaN)
+    #rms0 = np.sqrt(V0)
     #rms1 = fMap.ksmooth(rms0, K)
-    #V1 = rms1**1
+    #V1 = rms1**2
     #W1 = np.where(V1 > 0, 1.0 / V1, 0.0)
 
     # Smooth COVERAGE (same as BoA)
@@ -168,10 +168,6 @@ def auxsmoothby(m, Size=smoothby_deg):
     m.BeamSize = newbeam
     print('END SMOOTH')
 
-#def auxmapsum(m1, m2):
-#    '''
-#    Same as BoA but handles weighting correctly
-#     '''
     
 
 def auxwriteFits(data=None,outfile='boaMap.fits',overwrite=0,limitsX=[],limitsY=[],intensityUnit="Jy/beam",clip=-1):
