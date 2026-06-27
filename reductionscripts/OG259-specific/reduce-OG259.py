9c00c3a374dab60f5a5182d3428c67dbfc7bb1f2
vwoolvett <80853557+vwoolvett@users.noreply.github.com>
2026-06-26 22:40:40 -0400

    test

--- reductionscripts/OG259-specific/reduce-OG259.py
+++ reductionscripts/OG259-specific/reduce-OG259.py
@@ -94,32 +94,33 @@ def findSciTargetScans(source, obslogsdir, verbose=True):
                     line=lines[index]
                     index+=1 
                     if key == 'Scan':
-                        scan=int(line[4:-6])
-                        message+=(line[4:-6].ljust(6) + ' | ')  # 0
+                        scan_int = int(line[4:-6])
+                        scan = (line[4:-6].ljust(6) + ' | ')       # 0
                     if key == 'Source':
-                        message+=(line[4:-6].ljust(12) + ' | ') # 1     
+                        src = (line[4:-6].ljust(12) + ' | ')       # 1  
                     if key == 'Scan type':
-                        message+=(line[4:-6].ljust(12) + ' | ') # 2
+                        scantype = (line[4:-6].ljust(12) + ' | ')  # 2
                     if key == 'Observ. mode':
-                        message+=(line[4:-6].ljust(12) + ' | ') # 3
+                        mode = (line[4:-6].ljust(12) + ' | ')      # 3
                     if key == 'Scan duration':
-                        message+=(line[4:-6].ljust(12) + ' | ') # 4
+                        duration = (line[4:-6].ljust(12) + ' | ')  # 4
                     if key == 'Scan status':
-                        message+=(line[4:-6].ljust(12) + ' | ') # 5
+                        status = (line[4:-6].ljust(12) + ' | ')    # 5
                     if key == 'Comment':
-                        message+=(line[4:-6] + ' | ')           # 6
+                        comment = (line[4:-6] + ' | ')             # last
+                    message += scan + src + scantype + mode + duration + status
+
                 start = False
 
-                if source in message.split('|')[1]:
-                    if  '-999' not in message.split('|')[4]:
-                        if 'OTF' in message.split('|')[3] and 'OK' in message.split('|')[5]\
-                            and 'WARM' not in str.upper(message.split('|')[6]):
-                            message += 'SCAN CONSIDERED'
-                            scanlist.append(scan)
+                if source in src:
+                    if  '-999' not in duration:
+                        if 'OTF' in mode and 'OK' in status and 'warm' not in str.lower(comment):
+                            message += 'SCAN CONSIDERED | ' + comment
+                            scanlist.append(scan_int)
                         else:
-                            message += 'SCAN DISCARDED'
+                            message += 'SCAN DISCARDED | ' + comment
                     else:
-                        message += 'SCAN ONGOING'
+                        message += 'SCAN ONGOING | ' + comment
 
                     if verbose:
                         print(message)
