b4bfbac4eaf64776d8eecabf460f6aa8384a771f
vwoolvett <80853557+vwoolvett@users.noreply.github.com>
2026-06-26 23:01:02 -0400

    .

--- reductionscripts/OG259-specific/reduce-OG259.py
+++ reductionscripts/OG259-specific/reduce-OG259.py
@@ -107,11 +107,8 @@ def findSciTargetScans(source, obslogsdir, verbose=True):
                     if key == 'Scan status':
                         status = (line[4:-6].ljust(12) + ' | ')    # 5
                     if key == 'Comment':
-                        comment = (line[4:-6] + ' | ')             # last
-                    try:
+                        comment = (line[4:-6] + ' | ')             # last                        
                         message += scan + src + scantype + mode + duration + status
-                    except:
-                        pass
 
                 start = False
 
