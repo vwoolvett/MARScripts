4c2682010114fd63e1360f60d688a5ca16eb3421
vwoolvett <80853557+vwoolvett@users.noreply.github.com>
2026-07-01 22:39:34 -0400

    added skipMapping to redweak

--- reductionscripts/SPT3G/reduce-HFA-SPT3G.py
+++ reductionscripts/SPT3G/reduce-HFA-SPT3G.py
@@ -533,7 +533,7 @@ with warnings.catch_warnings():
 
                 # Reduce it
                 redweak(scan, fe=fe, size=-1, model=mymodel, subtract=subtract, doPlot=False, extremeFilter=False,
-                        writeSummary=writeSummary, flagJumps=flagJumps)
+                        writeSummary=writeSummary, flagJumps=flagJumps, skipMapping=True)
                 # NOTE: redweak's summary is everything about the timelines, nothing about map.
                 # NOTE 2: redweak then runs mapping in horizontal coords, forces a 10" (LFA) or 4.5"(HFA) smoothing
                 # and tries to solve for pointing corrections on smoothed map. Then prints timeline sensitivity
