65ef093eeb917df24450e48d015cf7d3fe79f90a
vwoolvett <80853557+vwoolvett@users.noreply.github.com>
2026-08-17 09:52:19 +0200

    added snr

--- reductionscripts/Template/createItermaps.py
+++ reductionscripts/Template/createItermaps.py
@@ -7,8 +7,10 @@ for i in range(1, niters+1):
     mymap = restoreFile(myname)
     mymaprms = copy.deepcopy(mymap)
     mymaprms.Data = np.where(mymaprms.Weight>0, 1.0/np.sqrt(mymaprms.Weight), 0)
+    mymapsnr  = copy.deepcopy(mymap)
+    mymapsnr.Data = mymap.Data/mymaprms.Data
     mymapcov = copy.deepcopy(mymap)
     mymapcov.Data = mymapcov.Coverage
 
-    mymaps['ITER%i'%i] = {'sig':mymap, 'rms':mymaprms, 'cov':mymapcov}
-    del mymap, mymaprms, mymapcov
+    mymaps['ITER%i'%i] = {'sig':mymap, 'rms':mymaprms, 'snr':mymapsnr, 'cov':mymapcov}
+    del mymap, mymaprms, mymapsnr, mymapcov
