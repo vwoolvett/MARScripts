0423a5708d50fa81e03d321b8225f5248f75aa56
vwoolvett <80853557+vwoolvett@users.noreply.github.com>
2026-08-17 09:19:39 +0200

    added script to create maps of iters

---
+++ reductionscripts/Template/createItermaps.py
@@ -0,0 +1,14 @@
+mymaps = {}
+for i in range(niters):
+    myname = 'ReducedFiles/%s-%s-%s'%(fe, source, system)
+    if flagJumps:
+        myname += '-flagJumps'
+    myname+='-coadded-flux-iter%i.data'%i
+    mymap = restoreFile(myname)
+    mymaprms = copy.deepcopy(mymap)
+    mymaprms.Data = np.where(mymaprms.Weight>0, 1.0/np.sqrt(mymaprms.Weight), 0)
+    mymapcov = copy.deepcopy(mymap)
+    mymapcov.Data = mymapcov.Coverage
+
+    mymaps['ITER%i'%i] = {'sig':mymap, 'rms':mymaprms, 'cov':mymapcov}
+    del mymap, mymapnoi, mymapcov
